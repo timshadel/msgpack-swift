df264891bf5a96fc2aaa8419b5a137d9e2173579
Tim Shadel <tim@shadelsoftware.com>
2016-01-04 14:24:52 -0700

    Unpack str family

--- Sources/str.swift
+++ Sources/str.swift
@@ -39,7 +39,39 @@ extension String: MsgPackValueType {
     }
 
     public static func unpack(data: NSData) throws -> MsgPackValueType {
-        throw MsgPackError.UnsupportedValue(data)
+        var type: UInt8 = 0
+        data.getBytes(&type, length: 1)
+
+        var start = 1
+        var length = 0
+        if type >= 0b10100000 && type < 0b11000000 {
+            length = Int(type) - Int(0b10100000)
+            guard data.length == length + 1 else { throw MsgPackError.UnsupportedValue(data) }
+        } else if type == 0xd9 {
+            start = 2
+            var value: UInt8 = 0
+            data.getBytes(&value, range: NSMakeRange(1, 1))
+            length = Int(value)
+            guard data.length == length + 2 else { throw MsgPackError.UnsupportedValue(data) }
+        } else if type == 0xda {
+            start = 3
+            var value: UInt16 = 0
+            data.getBytes(&value, range: NSMakeRange(1, 2))
+            length = Int(CFSwapInt16BigToHost(value))
+            guard data.length == length + 3 else { throw MsgPackError.UnsupportedValue(data) }
+        } else if type == 0xdb {
+            start = 5
+            var value: UInt32 = 0
+            data.getBytes(&value, range: NSMakeRange(1, 4))
+            length = Int(CFSwapInt32BigToHost(value))
+            guard data.length == length + 5 else { throw MsgPackError.UnsupportedValue(data) }
+        } else {
+            throw MsgPackError.UnsupportedValue(data)
+        }
+
+        let stringData = data.subdataWithRange(NSMakeRange(start, length))
+        guard let value = String(data: stringData, encoding: NSUTF8StringEncoding) else { throw MsgPackError.UnsupportedValue(data) }
+        return value
     }
 
 }
